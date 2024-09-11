5c5afaed1d5d2378c1104cde6d83407e5bf05a91
pathumck <ckpathumck@gmail.com>
2024-09-12 05:26:58 +0530

    make an unowned class a POJO

--- src/lk/ijse/spring/AppInitializer.java
+++ src/lk/ijse/spring/AppInitializer.java
@@ -1,6 +1,7 @@
 package lk.ijse.spring;
 
 import lk.ijse.spring.config.AppConfig;
+import lk.ijse.spring.pojo.BasicDataSource;
 import lk.ijse.spring.pojo.Customer;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -18,6 +19,13 @@ public class AppInitializer {
         Customer customer2 = ctx.getBean(Customer.class);
         System.out.println(customer2);
         customer2.getCustomerName();
+
+        //Making an unowned class a POJO
+        //@Bean annotation
+        BasicDataSource basicDataSource1 = ctx.getBean(BasicDataSource.class);
+        System.out.println(basicDataSource1);
+        BasicDataSource basicDataSource2 = ctx.getBean(BasicDataSource.class);
+        System.out.println(basicDataSource2);
 
         ctx.close();
     }
--- src/lk/ijse/spring/config/AppConfig.java
+++ src/lk/ijse/spring/config/AppConfig.java
@@ -1,5 +1,7 @@
 package lk.ijse.spring.config;
 
+import lk.ijse.spring.pojo.BasicDataSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,5 +10,10 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfig {
     public AppConfig() {
         System.out.println("App Config : Instantiated");
+    }
+
+    @Bean
+    public BasicDataSource basicDataSource() {
+        return new BasicDataSource();
     }
 }
