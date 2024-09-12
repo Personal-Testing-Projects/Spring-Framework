14ea0cc15570dae1a803945ffa42ec7024c543c7
pathumck <ckpathumck@gmail.com>
2024-09-12 05:55:40 +0530

    test registerShutdownHook

--- src/lk/ijse/spring/AppInitializer.java
+++ src/lk/ijse/spring/AppInitializer.java
@@ -27,6 +27,31 @@ public class AppInitializer {
         BasicDataSource basicDataSource2 = ctx.getBean(BasicDataSource.class);
         System.out.println(basicDataSource2);
 
-        ctx.close();
+        //ctx.close();
+
+        //After closing ctx above line will give an error
+        /*Customer customer3 = ctx.getBean(Customer.class);
+        System.out.println(customer3);*/
+
+        //For a solution for this we can use shutdown hook
+        /*Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("JVM is about to shutdown");
+                ctx.close();
+            }
+        }));
+
+        //Now above lines will be executed without an error
+        Customer customer4 = ctx.getBean(Customer.class);
+        System.out.println(customer4);*/
+
+        //Spring simplyfy above method
+        //This method will close the container just before JVM shutdown
+        ctx.registerShutdownHook();
+
+        //Now spring do the same process
+        Customer customer5 = ctx.getBean(Customer.class);
+        System.out.println(customer5);
     }
 }
